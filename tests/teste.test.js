74a952aecd80e7304a9b8b9eaa17944e078a4f60
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-29 18:48:46 -0300

    finalization of the back-end of the sign-up route. A sign-up route and a CRUD were created to add the new registered users from the users table. The validations are taking place through the Joi library and restrict the inputs in size, type and existence in the request body and the passwords are encrypted by the bcrypt library. Upon successful registration, the server returns 201, if validation fails, it returns 400, if the email is already registered, it returns 409 and other errors returns 500

--- tests/teste.test.js
+++
@@ -1,9 +0,0 @@
-import {app, connection} from '../src/app'
-import supertest from 'supertest';
-
-describe("GET /teste", () => {
-    it("returns 201 for tests", async () => {
-        const result = await supertest(app).get("/teste");
-        expect(result.status).toEqual(200);
-    });
-});
